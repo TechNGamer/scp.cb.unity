da0d8dc9f408b8eb50628e6b5c623d51563d8636
Jeff Crow <jeffwcrow@gmail.com>
2021-08-24 16:27:44 -0500

    Added some functionality to the door, more is to be added. But the main thing of the door auto-placing the button does work, and it's purpose is to allow flexibility to the door without having to deal with extra work wiring up a door to a button.

--- Assets/Script/Assembly-CSharp/Doors/Door.cs
+++ Assets/Script/Assembly-CSharp/Doors/Door.cs
@@ -6,6 +6,13 @@ namespace SCPCB.Remaster.Door {
 	[RequireComponent( typeof( Animator ) )]
 	public class Door : MonoBehaviour {
 		[SerializeField]
+		[Tooltip("Whether to use the location or not.")]
+		private bool useLocation0;
+		[SerializeField]
+		[Tooltip("Whether to use the location or not.")]
+		private bool useLocation1;
+		
+		[SerializeField]
 		[Tooltip("The button type to use for this kind of door.")]
 		private GameObject button;
 
@@ -19,13 +26,39 @@ namespace SCPCB.Remaster.Door {
 
 		private Animator animator;
 
-		// Start is called before the first frame update
 		private void Start() {
 			animator = GetComponent<Animator>();
+
+			if ( !button ) {
+				// Doing this to free up memory, even though it is a small amount of memory.
+				Destroy( button0Location );
+				Destroy( button1Location );
+
+				return;
+			}
+
+			// Checking if there is a location to place button.
+			if ( button0Location ) {
+				if ( useLocation0 ) {
+					Instantiate( button, button0Location );
+				} else {
+					Destroy( button0Location );
+				}
+			}
+
+			// Disabling in case more is to be added here.
+			// ReSharper disable once InvertIf
+			if ( button1Location && useLocation1 ) {
+				if ( useLocation1 ) {
+					Instantiate( button, button1Location );
+				} else {
+					Destroy( button1Location );
+				}
+			}
 		}
 
 		// Update is called once per frame
-		void Update() {
+		private void Update() {
 		}
 	}
 }
