f27095b8647db8150096828db96bb6057eeba1d8
Jeff Crow <jeffwcrow@gmail.com>
2021-09-27 11:14:58 -0500

    Moved some files around, added another room to the testing scene. Also attempting to make an RMesh Importer for Unity.

---
+++ Assets/Script/Editor/RMeshImporter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCPCB.Remaster
+{
+    public class RMeshImporter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
