bab01abfcd854e4fbaf9c9c9491052921afed0be
Jeff Crow <jeffwcrow@gmail.com>
2021-10-08 22:48:09 -0500

    Added the WebAudio library for Android and WebGL builds.

---
+++ Assets/Script/SCPCB.Remaster.Audio/Web/WebAudio.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SCPCB.Remaster.Audio {
+	public class WebAudio : Audio {
+		public static readonly Uri STREAMING_ASSETS = new Uri( Application.streamingAssetsPath );
+		
+		public WebAudio( string subPath ) {
+			if ( subPath.StartsWith( "/" ) ) {
+				subPath = subPath.Remove( 0, 1 );
+			}
+
+			var webRequest = UnityWebRequestMultimedia.GetAudioClip( new Uri( STREAMING_ASSETS, subPath ), AudioType.MPEG );
+			var request    = webRequest.SendWebRequest();
+
+			while ( !request.isDone ) {
+				Task.Delay( 256 ).GetAwaiter().GetResult();
+			}
+
+			Clip = ( ( DownloadHandlerAudioClip )webRequest.downloadHandler ).audioClip;
+
+			IsStreamed = false;
+		}
+	}
+}
