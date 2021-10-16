9baf2e2577d40ac4cf9f390d94ca2612f7fd9227
Jeff Crow <jeffwcrow@gmail.com>
2021-10-15 23:03:21 -0500

    Added a required component (AudioSource) to the Button. Also made it play a sound if it is not interactable or is, along with invoking the UnityEvent if it is interactable.

--- Assets/Script/Assembly-CSharp/Map/Doors/Button.cs
+++ Assets/Script/Assembly-CSharp/Map/Doors/Button.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using SCPCB.Remaster.Audio;
 using SCPCB.Remaster.Map;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace SCPCB.Remaster.Door {
+	[RequireComponent(typeof(AudioSource))]
 	public class Button : MonoBehaviour, IInteractable {
 
 		// Disabling this for the reason that this boolean must be toggleable/saved via Unity.
@@ -24,8 +26,25 @@ namespace SCPCB.Remaster.Door {
 		[SerializeField]
 		public UnityEvent ButtonPressed;
 
+		private AudioSource  source;
+		private AudioManager audioManager; 
+
+		private void Start() {
+			audioManager = AudioManager.Singleton;
+
+			source = GetComponent<AudioSource>();
+		}
+
 		public virtual void Interact() {
+			if ( !Interactable ) {
+				source.clip = audioManager["Interact"]["ButtonFailed"].Clip;
+			} else {
+				ButtonPressed?.Invoke();
 			
+				source.clip = audioManager["Interact"]["ButtonSuccess"].Clip;
+			}
+
+			source.Play();
 		}
 	}
 }
