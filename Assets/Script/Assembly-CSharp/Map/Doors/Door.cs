a3ab2ee9982d5ca13434016803d2e3a9e5a174af
Jeff Crow <jeffwcrow@gmail.com>
2021-10-15 23:06:31 -0500

    Made door require an Audio Source. Also made it play a sound when it opens and closes.

--- Assets/Script/Assembly-CSharp/Map/Doors/Door.cs
+++ Assets/Script/Assembly-CSharp/Map/Doors/Door.cs
@@ -1,9 +1,11 @@
 using System;
+using SCPCB.Remaster.Audio;
 using SCPCB.Remaster.Map;
 using UnityEngine;
 
 namespace SCPCB.Remaster.Door {
 	[RequireComponent( typeof( Animator ) )]
+	[RequireComponent( typeof( AudioSource ) )]
 	public class Door : MonoBehaviour {
 		private static readonly int OPEN  = Animator.StringToHash( "Open" );
 		private static readonly int CLOSE = Animator.StringToHash( "Close" );
@@ -16,16 +18,26 @@ namespace SCPCB.Remaster.Door {
 		[Tooltip( "The location of the buttons." )]
 		private Button[] buttons;
 
-		private Animator animator;
+		private Animator    animator;
+		private AudioSource source;
 
 		public void ToggleDoor() {
+			if ( animator.GetCurrentAnimatorStateInfo( 0 ).normalizedTime < 1f ) {
+				return;
+			}
+
 			doorOpen = !doorOpen;
 
 			animator.SetTrigger( doorOpen ? OPEN : CLOSE );
+
+			source.clip = AudioManager.Singleton["Door"][doorOpen ? "DoorOpen1" : "DoorClose1"].Clip;
+
+			source.Play();
 		}
 
 		private void Start() {
 			animator = GetComponent<Animator>();
+			source   = GetComponent<AudioSource>();
 
 			if ( doorOpen ) {
 				animator.SetTrigger( OPEN );
