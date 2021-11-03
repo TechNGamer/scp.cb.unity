47a8398ca8b0cb9b89ace8cbe25b38fca44d9d92
Jeff Crow <jeffwcrow@gmail.com>
2021-11-02 22:38:28 -0500

    Forgot to add SCP-066's class to the commit.

---
+++ Assets/Script/Assembly-CSharp/NPC/IdleStateManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace SCPCB.Remaster {
+	public class IdleStateManager : StateMachineBehaviour {
+		private static readonly int IDLE = Animator.StringToHash( "Idle" );
+
+		// OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+		public override void OnStateUpdate( Animator animator, AnimatorStateInfo stateInfo, int layerIndex ) {
+			if ( stateInfo.length >= 0.95f ) {
+				return;
+			}
+
+			var nextIdle = Random.Range( 0f, 1f );
+
+			if ( nextIdle > 0.9f ) {
+				animator.SetInteger( IDLE, 2 );
+			} else if ( nextIdle > 0.8f ) {
+				animator.SetInteger( IDLE, 1 );
+			} else {
+				animator.SetInteger( IDLE, 0 );
+			}
+		}
+	}
+}
