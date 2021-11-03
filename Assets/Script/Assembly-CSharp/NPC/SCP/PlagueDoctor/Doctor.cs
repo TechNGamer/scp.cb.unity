8b064ee43d02a7ab68da6ec6b5015de26f85a381
Jeff Crow <jeffwcrow@gmail.com>
2021-11-03 14:45:47 -0500

    Started work on the logic of the plague doctor.

---
+++ Assets/Script/Assembly-CSharp/NPC/SCP/PlagueDoctor/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SCPCB.Remaster.Player;
+using UnityEngine;
+
+namespace SCPCB.Remaster.NPC.SCP.PlagueDoctor {
+	[RequireComponent( typeof( Animator ) )]
+	public class Doctor : MonoBehaviour {
+		private PlayerController player;
+		private Animator         anim;
+
+		private void Start() {
+			player = PlayerController.Player;
+			anim   = GetComponent<Animator>();
+		}
+
+		private void Update() {
+		}
+	}
+}
