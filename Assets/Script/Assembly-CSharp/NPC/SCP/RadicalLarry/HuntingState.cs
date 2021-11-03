35fc40e454b17cf3738ed658aa7b57135ed28cd8
Jeff Crow <jeffwcrow@gmail.com>
2021-11-03 13:01:39 -0500

    Got the animation controller for SCP-106, along with adding some logic, and putting it on SCP-106.

---
+++ Assets/Script/Assembly-CSharp/NPC/SCP/RadicalLarry/HuntingState.cs
@@ -0,0 +1,23 @@
+﻿using SCPCB.Remaster.Player;
+using UnityEngine;
+
+// This class is used to make SCP-106 hunt the player.
+public class HuntingState : StateMachineBehaviour {
+	private static readonly int STOP_HUNTING = Animator.StringToHash( "StopHunting" );
+
+	private PlayerController player;
+
+	public override void OnStateEnter( Animator animator, AnimatorStateInfo stateInfo, int layerIndex ) {
+		player = PlayerController.Player;
+	}
+
+	public override void OnStateUpdate( Animator animator, AnimatorStateInfo stateInfo, int layerIndex ) {
+		if ( player == null || !player ) {
+			animator.SetTrigger( STOP_HUNTING );
+
+			return;
+		}
+		
+		animator.transform.LookAt( player.transform );
+	}
+}
