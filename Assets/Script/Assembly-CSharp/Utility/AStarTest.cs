d1f49096fdf2e5fcd221d052a94b0fce20c313c3
Jeff Crow <jeffwcrow@gmail.com>
2021-11-08 18:17:33 -0600

    Changed AStarTest.cs to make a lot more sense. This class probably should not exist later on, is merely meant for testing.

--- Assets/Script/Assembly-CSharp/Utility/AStarTest.cs
+++ Assets/Script/Assembly-CSharp/Utility/AStarTest.cs
@@ -15,6 +15,8 @@ namespace SCPCB.Remaster.Utility {
 		public CancellationTokenSource       tokenSource;
 		public Coroutine                     coroutine;
 
+		private AStarGridManager.Node[] walkTos;
+
 		public void Start() {
 			oldPos      = target.transform.position;
 			tokenSource = new CancellationTokenSource();
@@ -23,26 +25,47 @@ namespace SCPCB.Remaster.Utility {
 		}
 
 		public void Update() {
-			if ( oldPos != target.transform.position || task.IsFaulted ) {
-				tokenSource.Cancel( false );
+			if ( oldPos != target.transform.position ) {
+				if ( coroutine != null ) {
+					StopCoroutine( coroutine );
+
+					coroutine = null;
+				}
+
+				if ( task.Status == TaskStatus.Running ) {
+					tokenSource.Cancel( false );
+				}
 
 				Start();
+
+				return;
 			}
 
-			if ( !task.IsFaulted && task?.Result != null && task.IsCompleted && coroutine == null ) {
-				coroutine = StartCoroutine( WalkToTarget( task.Result ) );
+			if ( task.Status == TaskStatus.RanToCompletion && coroutine == null ) {
+				walkTos = task.Result;
+
+				coroutine = StartCoroutine( WalkToTarget() );
 			}
 		}
 
-		public IEnumerator WalkToTarget( AStarGridManager.Node[] nodes ) {
-			foreach ( var node in nodes ) {
+		public IEnumerator WalkToTarget() {
+			for ( var i = 0; i < walkTos.Length; ++i ) {
+				var node = walkTos[i];
+
 				while ( transform.position != node.worldPosition ) {
 					yield return new WaitForEndOfFrame();
-					
+
 					var myTransform = transform;
 					var position    = myTransform.position;
+					var heading     = position - node.worldPosition;
+					var distance    = heading.magnitude;
+					var direction   = heading / distance;
 
-					position += ( position - node.worldPosition ) * Time.deltaTime;
+					if ( distance < 0.5f ) {
+						break;
+					}
+
+					position -= direction * 3f * Time.deltaTime;
 
 					myTransform.position = position;
 				}
