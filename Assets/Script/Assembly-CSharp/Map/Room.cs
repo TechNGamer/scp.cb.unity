3f01133ff4f2a4def86e532417e9837a3c26eeb3
Jeff Crow <jeffwcrow@gmail.com>
2021-11-12 18:16:09 -0600

    Started work on room loading. Made a class who's sole purpose is to contain the data, and reference to, the room.

---
+++ Assets/Script/Assembly-CSharp/Map/Room.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SCPCB.Remaster.Map {
+	public class Room {
+
+		public float SpawnChance {
+			get;
+			private set;
+		}
+
+		public string Name {
+			get;
+			private set;
+		}
+
+		public GameObject GameRoom {
+			get;
+			private set;
+		}
+
+		internal Room( float chance, string name, string assetName ) {
+			
+		}
+	}
+}
---
+++ Assets/Script/Assembly-CSharp/Map/RoomManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCPCB.Remaster.Map {
+	public static class RoomManager {
+
+		private static Dictionary<string, GameObject> rooms;
+
+	}
+}
